19a04a744d852302a63ad2351596ca155df83efc
joaquin-ruiz-masmovil <joaquin.ruiz@masmovil.com>
2021-02-24 13:53:08 +0100

    Add GetPhoneCatalogService Test

--- src/test/java/es/codetest/phoneapp/application/getphonecatalog/GetPhoneCatalogServiceShould.java
+++ src/test/java/es/codetest/phoneapp/application/getphonecatalog/GetPhoneCatalogServiceShould.java
@@ -1,10 +1,22 @@
 package es.codetest.phoneapp.application.getphonecatalog;
 
+import es.codetest.phoneapp.domain.model.CatalogPhone;
 import es.codetest.phoneapp.domain.repository.CatalogPhoneRepository;
+import es.codetest.phoneapp.mother.CatalogPhoneMother;
+import io.reactivex.Single;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GetPhoneCatalogServiceShould {
@@ -18,22 +30,22 @@ public class GetPhoneCatalogServiceShould {
   public void setUp() {
     getPhoneCatalogService = new GetPhoneCatalogService(catalogPhoneRepositoryMock);
   }
-/*
+
   @Test
   public void returnAllPhonesCatalog() {
     // given
-    Single<List<CatalogPhone>> phoneCatalog = CatalogPhoneMother.generatePhoneCatalog();
+    Single<List<CatalogPhone>> phoneCatalog = Single.just(CatalogPhoneMother.generatePhoneCatalog());
     when(catalogPhoneRepositoryMock.getPhoneCatalog())
         .thenReturn(phoneCatalog);
 
     // when
-    List<PhoneCatalogInfoDTO> result = getPhoneCatalogService.execute();
+    Single<List<PhoneCatalogInfoDTO>> result = getPhoneCatalogService.execute();
 
     // then
-    List<String> catalogPhoneNames = result.stream()
+    List<String> catalogPhoneNames = result.blockingGet().stream()
         .map(p -> p.getName())
         .collect(Collectors.toList());
-    assertEquals(result.size(), phoneCatalog.size());
+    assertEquals(result.blockingGet().size(), catalogPhoneNames.size());
     assertTrue(catalogPhoneNames.contains("Ericsson"));
     assertTrue(catalogPhoneNames.contains("Nokia"));
     assertTrue(catalogPhoneNames.contains("iPhone"));
@@ -52,11 +64,11 @@ public class GetPhoneCatalogServiceShould {
         .thenReturn(Single.just(phoneCatalog));
 
     // when
-    List<PhoneCatalogInfoDTO> result = getPhoneCatalogService.execute();
+    Single<List<PhoneCatalogInfoDTO>> result = getPhoneCatalogService.execute();
 
     // then
-    assertEquals(result.size(), 0);
+    assertEquals(result.blockingGet().size(), 0);
 
   }
-*/
+
 }
